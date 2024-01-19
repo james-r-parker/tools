daeea0c0bacbc5974430b1c22924c55bbc6dfad6
James Parker <j1mb0jay@jimbojay.com>
2024-01-19 09:09:09 +0000

    switch to tailwind

--- src/DotnetHelp.DevTools.Web/Program.cs
+++ src/DotnetHelp.DevTools.Web/Program.cs
@@ -2,7 +2,6 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DotnetHelp.DevTools.Web;
-using DotnetHelp.DevTools.Web.Application;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
