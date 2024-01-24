9d087a6cc97d2633e5d7a74e6ada578cd85f0a0e
James Parker <j1mb0jay@jimbojay.com>
2024-01-24 06:55:06 +0000

    cache table

--- src/DotnetHelp.DevTools.Api.Cdk/src/DotnetHelpDevToolsApiStack.cs
+++ src/DotnetHelp.DevTools.Api.Cdk/src/DotnetHelpDevToolsApiStack.cs
@@ -39,7 +39,7 @@ public class DotnetHelpDevToolsApiStack : Stack
             {
                 { "WEBSOCKET_URL", Fn.ImportValue("DOTNETHELP:DEVTOOLS:WSS:URL") },
                 { "CONNECTION_TABLE_NAME", Fn.ImportValue("DOTNETHELP:DEVTOOLS:WSS:TABLE") },
-                { "CACHE_TABLE_NAME", Fn.ImportValue("DOTNETHELP:DEVTOOLS:INFRASTRUCTURE:CACHE:TABLE") },
+                { "CACHE_TABLE_NAME", Fn.ImportValue("DOTNETHELP:DEVTOOLS:INFRASTRUCTURE:DISTRIBUTEDCACHE:TABLE") },
                 { "AWS_STS_REGIONAL_ENDPOINTS", "regional" },
                 { "BIN_TABLE_NAME", Fn.ImportValue("DOTNETHELP:DEVTOOLS:INFRASTRUCTURE:BIN:TABLE") }
             }
