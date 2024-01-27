eb477e08fe9cc595dace63e3d17bd4f324b84a5a
James Parker <j1mb0jay@jimbojay.com>
2024-01-27 08:04:50 +0000

    new db table with index

--- src/DotnetHelp.DevTools.Infrastructure.Cdk/src/DotnetHelpDevToolsInfrastructureStack.cs
+++ src/DotnetHelp.DevTools.Infrastructure.Cdk/src/DotnetHelpDevToolsInfrastructureStack.cs
@@ -1,9 +1,6 @@
-using System.Collections.Generic;
 using Amazon.CDK;
-using Amazon.CDK.AWS.CodeDeploy;
 using Amazon.CDK.AWS.DynamoDB;
 using Amazon.CDK.AWS.IAM;
-using Amazon.CDK.AWS.Lambda;
 using Constructs;
 
 namespace DotnetHelp.DevTools.Infrastructure.Cdk;
@@ -22,8 +19,19 @@ public class DotnetHelpDevToolsInfrastructureStack : Stack
             PointInTimeRecovery = false,
             TimeToLiveAttribute = "ttl",
         });
+        
+        var db = new Table(this, "DnhDb", new TableProps()
+        {
+            PartitionKey = new Attribute() { Name = "bucket", Type = AttributeType.STRING },
+            SortKey = new Attribute() { Name = "created", Type = AttributeType.NUMBER },
+            BillingMode = BillingMode.PAY_PER_REQUEST,
+            RemovalPolicy = RemovalPolicy.DESTROY,
+            Encryption = TableEncryption.AWS_MANAGED,
+            PointInTimeRecovery = false,
+            TimeToLiveAttribute = "ttl",
+        });
 
-        binTable.AddLocalSecondaryIndex(new LocalSecondaryIndexProps()
+        db.AddLocalSecondaryIndex(new LocalSecondaryIndexProps()
         {
             SortKey = new Attribute() { Name = "slug", Type = AttributeType.STRING },
             IndexName = "ix_bucket_slug",
@@ -42,20 +50,27 @@ public class DotnetHelpDevToolsInfrastructureStack : Stack
         var dataPolicy = new ManagedPolicy(this, "DatabasePolicy", new ManagedPolicyProps());
         binTable.GrantReadWriteData(dataPolicy);
         cacheTable.GrantReadWriteData(dataPolicy);
+        db.GrantReadWriteData(dataPolicy);
         
-        new CfnOutput(this, "BIN_TABLE", new CfnOutputProps
+        _ = new CfnOutput(this, "DB_TABLE", new CfnOutputProps
+        {
+            Value = db.TableName,
+            ExportName = "DOTNETHELP:DEVTOOLS:INFRASTRUCTURE:DB:TABLE",
+        });
+        
+        _ = new CfnOutput(this, "BIN_TABLE", new CfnOutputProps
         {
             Value = binTable.TableName,
             ExportName = "DOTNETHELP:DEVTOOLS:INFRASTRUCTURE:BIN:TABLE",
         });
         
-        new CfnOutput(this, "CACHE_TABLE", new CfnOutputProps
+        _ = new CfnOutput(this, "CACHE_TABLE", new CfnOutputProps
         {
             Value = cacheTable.TableName,
             ExportName = "DOTNETHELP:DEVTOOLS:INFRASTRUCTURE:CACHE:TABLE",
         });
 
-        new CfnOutput(this, "BIN_DB_POLICY", new CfnOutputProps
+        _ = new CfnOutput(this, "BIN_DB_POLICY", new CfnOutputProps
         {
             Value = dataPolicy.ManagedPolicyName,
             ExportName = "DOTNETHELP:DEVTOOLS:INFRASTRUCTURE:DB:POLICY",
